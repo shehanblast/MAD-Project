ed3eec778d3dc96765fd88548209a1841f047de0
shehanblast <shihanbinuka@gmail.com>
2020-10-03 23:04:06 +0530

    Add Testing

--- app/src/androidTest/java/com/example/dbdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/dbdemo/ExampleInstrumentedTest.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,6 +18,27 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private EventPlanner eventPlanner;
+
+    @Before
+    public void setUp(){
+        eventPlanner = new EventPlanner();
+    }
+
+    @Test
+    public void hours(){
+        int result = eventPlanner.convertIntoHours(120);
+        assertEquals(2,result);
+    }
+
+    @Test
+    public void seconds(){
+        int result = eventPlanner.convertIntoHours(120);
+        assertEquals(7200,result);
+    }
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
